6e288b67e919c1749ea59156a4e287035ef6c8e2
Max Dokuchaev <mixamus@icloud.com>
2021-10-16 23:13:58 +0300

    Added annotation lazy value false

--- src/main/java/com/example/springboot/domain/Shop.java
+++ src/main/java/com/example/springboot/domain/Shop.java
@@ -3,8 +3,10 @@ package com.example.springboot.domain;
 import com.example.springboot.interfaces.Price;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+@Lazy(value = false)
 @Slf4j
 @Component
 public class Shop {
